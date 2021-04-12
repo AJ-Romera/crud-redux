aab3af7af56c0cc41ca326e2ed2c996f493490f3
AJ-Romera <ajromeraalfonso@gmail.com>
2021-04-12 12:11:50 +0200

    CHNG: Crea Routing del Proyecto && Varios Componentes

---
+++ src/components/EditarProducto.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function EditarProducto() {
+    return <h1>Desde Componente EditarProductos.js</h1>;
+}
+
+export default EditarProducto;
---
+++ src/components/NuevoProducto.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Productos() {
+    return <h1>Desde Componente NuevoProducto.js</h1>;
+}
+
+export default Productos;
